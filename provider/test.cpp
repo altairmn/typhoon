98528ea5d03709b73a0d9e5ec55449fa2ea8b956
altairmn <nukeballzy@gmail.com>
2018-08-17 11:55:04 -0400

    Changed makefile

--- provider/test.cpp
+++ provider/test.cpp
@@ -1,28 +1,15 @@
 // testing condition variable
 #include <iostream>
-#include <mutex>
-#include <chrono>
-#include <condition_variable>
-#include <json.hpp>
-#include <zmq.hpp>
-#include <cstdio>
-
-using json = nlohmann::json;
-
-struct tick {
-  tick() : p1(0.0), p2(0.0), p1_set(0), p2_set(0) {}
-  float p1, p2;
-  bool p1_set, p2_set;
-};
-
-std::map <long long int, tick> mp;
+#include <string>
+#include <fmt/core.h>
 
 int main(int argc, char* argv[]) {
 
-  for(int i = 0; i < 1000; ++i) {
-    int f = rand() % 100;
-    mp.emplace(f, tick());
-  }
+  std::string ts = "\"2018-08-17T04:20:14.837Z\"";
+  std::tm t = {};
+  auto gts = [](std::string ts) -> std::string {
 
+  }
+  std::istringstream ss(ts);
   return 0;
 }
